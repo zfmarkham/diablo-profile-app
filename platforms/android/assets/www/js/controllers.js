a7cb10ca4a88a4869b985b061e7d74d3298d289f
zmarkham <zmarkham@outlook.com>
2016-01-25 18:19:43 +0000

    Moved existing files to new location

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,45 @@
+angular.module('starter.controllers', [])
+
+.controller('SearchCtrl', function ($scope, $http, $state) {
+
+    $scope.search = function (bnetId) {
+
+        $http.get('https://eu.api.battle.net/d3/profile/' + bnetId + '/?locale=en_GB&apikey=u5trkg9h53u2vv9rk6fwgxecwrkvgs8s')
+           .success(function (data) {
+               $state.go('tab.dash', { profile: data });
+           })
+           .error(function (err) {
+               return err;
+           });
+    };
+})
+
+.controller('DashCtrl', function ($scope, profile) {
+    profile.success(function (data) {
+        $scope.profile = data;
+
+        // This is needed to circumvent the issue of a JSON property containing a hyphen
+        $scope.orderBy = function (property) {
+            return function (item) {
+                return item[property];
+            };
+        };
+    });
+})
+
+.controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    }
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
